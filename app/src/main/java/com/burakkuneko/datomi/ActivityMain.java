4f073516191702eae01e05da91adc59f7622da57
enmanuel <enmanuelgutierrezpoey@gmail.com>
2024-11-13 02:25:31 -0500

    Defining telephonyManager.sendUssdRequest
    handler its important, so its can update anything inside onReceiveUssdResponse

--- app/src/main/java/com/burakkuneko/datomi/ActivityMain.java
+++ app/src/main/java/com/burakkuneko/datomi/ActivityMain.java
@@ -1,27 +1,36 @@
 package com.burakkuneko.datomi;
-import android.net.TrafficStats;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Looper;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import androidx.core.app.ActivityCompat;
 
-public class ActivityMain extends Activity{
+public class ActivityMain extends Activity {
     int counter = 0;
+    TelephonyManager telephonyManager;
     LinearLayout main;
     TextView textView;
     Button button;
 
+    Handler handler = new Handler(Looper.getMainLooper());
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
+        telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
 
         main = findViewById(R.id.main);
         textView = findViewById(R.id.text);
@@ -29,12 +38,32 @@ public class ActivityMain extends Activity{
         button.setOnClickListener(requestUssdCall());
 
     }
+
     View.OnClickListener requestUssdCall() {
         return new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-
+            public void onClick(View view) {
+                sendUssdRequest();
             }
         };
     }
+
+    void sendUssdRequest() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            telephonyManager.sendUssdRequest("*222#", new TelephonyManager.UssdResponseCallback() {
+                @Override
+                public void onReceiveUssdResponse(TelephonyManager telephonyManager, String request, CharSequence response) {
+                    super.onReceiveUssdResponse(telephonyManager, request, response);
+                    String res = response.toString();
+                    Log.d("USSD_RESPONSE: ", res);
+                    textView.setText(res);
+                    Toast.makeText(ActivityMain.this, response, Toast.LENGTH_LONG).show();
+                }
+            }, handler);
+        }
+    }
+
 }
